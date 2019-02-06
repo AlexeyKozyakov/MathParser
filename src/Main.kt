2e7e387ba319b5fe7caa8491b1c8fc723d56e017
AlexeyKozyakov <xelexerx@gmail.com>
2019-02-07 00:03:45 +0700

    add: expression example

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
-fun main() {
+import mathparser.Lexer
+import mathparser.Parser
+import java.io.StringReader
 
+fun main() {
+    val expression = "((-1 * 3 ^ 2 + (3 - 43)) * 3 - 1) / 2"
+    val parser = Parser(Lexer(StringReader(expression)))
+    print("((-1 * 3 ^ 2 + (3 - 43)) * 3 - 1) / 2 =  ${parser.parse()}")
 }
